b740a6b08d9bc97cbebc764fb913a24d90f6e74b
shubhamprashar <shubham.prashar@finbox.in>
2024-05-18 00:17:56 +0530

    Initial Commit

---
+++ menu/menuApp.swift
@@ -0,0 +1,17 @@
+//
+//  menuApp.swift
+//  menu
+//
+//  Created by Shubham Prashar on 18/05/24.
+//
+
+import SwiftUI
+
+@main
+struct menuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
